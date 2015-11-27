ede28880992bfa9cd6a82c0281be8089b4ab422e
heojae91 <heojae91@gmail.com>
2015-11-27 15:19:58 +0900

    이거

--- index.php
+++ index.php
@@ -3,23 +3,23 @@
     <head>
         <title>로그인 페이지</title>
         <meta charset="utf-8" >
-        <link rel="stylesheet" href="common/css/index.css"/>
-        <link rel="stylesheet" href="common/css/font-awesome.min.css">
-        <link rel="stylesheet" href="common/css/bootstrap.min.css">
-  		<link rel="stylesheet" href="common/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="/common/css/index.css"/>
+        <link rel="stylesheet" href="/common/css/font-awesome.min.css">
+        <link rel="stylesheet" href="/common/css/bootstrap.min.css">
+  		<link rel="stylesheet" href="/common/css/bootstrap-theme.min.css">
     </head>
     <body>
 		<header>
         </header>
-    	<div id="login_logo"></div>
+    	<div id="login_logo">
+    	<img src = "loginlogo.png" alt="longlogo" width="450px" height ="200" />
+		</div>
         <div id="login_window">
 	        <form name="login_form" method="post" font="12px">
 	        	<br />
-	        	<br />
-	        	<br />
-	        	<br />
-	            <p class="id_pw">I  D : <input class="id_pw_input" type="text" name="user_id"/></p><br />
-	            <p class="id_pw">PW : <input class="id_pw_input" type="password" name="user_pass" /></p><br />
+	  	       	<br />
+	            <p class="id_pw"><input class="id_pw_input" type="text" name="user_id" placeholder =" ID "/></p><br />
+	            <p class="id_pw"><input class="id_pw_input" type="password" name="user_pass" placeholder =" Password "/></p><br />
 	            <br />
 	            <button class="btn btn-info">비번찾기</button>
 				<button class="btn btn-info">회원가입</button>
