203619de435ed51128ab563c43f737dbfda6e4ca
heojae91 <heojae91@gmail.com>
2015-12-13 17:03:02 +0900

    우클릭테스트 + feedback.php 수정중

--- common/js/contextMenu.js
+++ common/js/contextMenu.js
@@ -1,21 +1,33 @@
-$(function() {
-    var $contextMenu = $("#contextMenu");
+document.observe("dom:loaded", function() {
+    var contextMenu = $("contextMenu");
+    var coordX = 0;
+    var coordY = 0;
+    
+    $("plz").observe("contextmenu", function(e){
+        e.stop();
+    });
 
-    $("html").on("contextmenu",function(e) {
-        $contextMenu.css({
+    $("plz").observe("contextmenu", function(e) {
+        coordX = e.pointerX();
+        coordY = e.pointerY();
+        contextMenu.setStyle({
             display: "block",
-            left: e.pageX,
-            top: e.pageY
+            left: coordX + "px",
+            top: coordY + "px"
         });
         return false;
     });
-
-    $("html").on("click", function() {
-        $contextMenu.hide();
-    })
-
-    $contextMenu.on("click", "a", function(e) {
-        alert(e.pageX + ", " + e.pageY);
-        $contextMenu.hide();
+    
+    document.observe("click", function() {
+        contextMenu.setStyle({
+            display: "none"
+        });
+    });
+    
+    $("write").observe("click", function() {
+        alert("click");
+        contextMenu.setStyle({
+            display: "none"
+        });
     });
 });
